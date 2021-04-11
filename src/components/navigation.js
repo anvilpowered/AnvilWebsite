36974dfb0cf33e1bd837a6748991ce47ef88200d
STG-Allen <ajacob715@gmail.com>
2021-04-11 14:21:13 -0400

    Added notebox css, minor changes

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,14 +1,13 @@
 import React from "react"
-import {Link} from "gatsby"
 import ThemeChanger from "../components/themeChanger"
 
 export default (props) => (
   <nav className="navigation">
-    <a href="https://www.anvilpowered.org/">Documentation</a>
+    <a href={"https://www.anvilpowered.org/"}>Documentation</a>
     <b>|</b>
-    <a href="https://discord.com/invite/6gR2YH3">Discord</a>
+    <a href={"https://discord.com/invite/6gR2YH3"}>Discord</a>
     <b>|</b>
-    <Link to={"/temp"}>Github</Link>
+    <a href={"https://github.com/anvilpowered/"}>Github</a>
     <ThemeChanger/>
   </nav>
 )
